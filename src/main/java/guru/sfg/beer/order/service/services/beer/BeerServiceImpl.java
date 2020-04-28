a3ce937884b22eea5159ae7a6c3a1a2c699fd9ae
Jaakko <neneth2@gmail.com>
2020-04-28 15:09:57 +0300

    Add spring profile. Fix hibernate errors with mysql. Add mysql init script.

--- src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.sfg.beer.order.service.services.beer;
 
 import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 @Service
 public class BeerServiceImpl implements BeerService {
@@ -30,6 +32,7 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public Optional<BeerDto> getBeerByUpc(String upc) {
+        log.info("Get beer from beer service by UPC: {}", upc);
         return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_UPC_PATH_V1 + upc, BeerDto.class));
     }
 
